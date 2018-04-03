12c6180bc554430ac513312f18b9de4bac7a2559
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-04-03 16:48:34 -0700

    Add files via upload

---
+++ model.js/article (2).js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var articleSchema= new Schema({
+    headline: String,
+    byline: String,
+    sectionName: String,
+    pubDate: String, 
+    webURL: String,
+    dateSaved: {type: Date, default: Date.now},
+    deleted: {type: Boolean, default: false}
+});
+
+var ArticleModel = mongoose.model('Article',articleSchema);
+
+module.exports=ArticleModel; 
